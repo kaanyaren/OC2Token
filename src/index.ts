6ae06c188a693aac6babe06ee2ad7bc9846bc518
kaanyaren <kaanyaren@gmail.com>
2026-09-02 13:03:44 +0300

    chore: expose public module barrel

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 export * from "./application.js";
 export * from "./cache/index.js";
-export * from "./collector/index.js";
-export * from "./dashboard/state/index.js";
+export * from "./collector/collector.js";
+export { intervalUnion } from "./collector/types.js";
+export type { CollectorOptions } from "./collector/types.js";
+export * from "./dashboard/state/refresh-coordinator.js";
 export * from "./domain/index.js";
 export * from "./opencode/index.js";
 export * from "./output/index.js";
